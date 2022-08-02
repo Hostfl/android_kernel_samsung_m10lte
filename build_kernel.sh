900800ed315fbd5546d6ef0d9f83dab3da2d0209
Hostfl <108982682+Hostfl@users.noreply.github.com>
2022-08-02 17:56:51 +0100

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,3 +7,6 @@ export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 m10ltesea_00_defconfig
 make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 -j30
+
+cp arch/arm64/boot/Image AK/
+zip "PKernel.zip" "/home/runner/kernel/AK/*"
