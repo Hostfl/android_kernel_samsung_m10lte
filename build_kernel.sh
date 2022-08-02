b3fa484716c90e0361dbb6cfed9708c52f09256a
Hostfl <108982682+Hostfl@users.noreply.github.com>
2022-08-02 15:48:36 +0100

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,10 +4,11 @@ export ARCH=arm64
 export PLATFORM_VERSION=10
 export ANDROID_MAJOR_VERSION=q
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+export AK=$(pwd)
 
 make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 m10ltesea_00_defconfig
-make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 -j16
+make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 -j300
 
-mv arch/arm64/boot/Image $(pwd)/AK
+mv arch/arm64/boot/Image $AK/AK
 
-zip PKernel.zip $(pwd)/AK
+zip PKernel.zip $AK/AK
