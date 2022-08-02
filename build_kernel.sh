0e723eb99b9c7b8d47ae6c0836c5c35f64187608
Hostfl <108982682+Hostfl@users.noreply.github.com>
2022-08-02 16:37:32 +0100

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,11 +4,11 @@ export ARCH=arm64
 export PLATFORM_VERSION=10
 export ANDROID_MAJOR_VERSION=q
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
-export AK=$(pwd)
 
 make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 m10ltesea_00_defconfig
 make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 -j30
 
-mv "arch/arm64/boot/Image" "$AK/AK"
+mv "arch/arm64/boot/Image" "$(pwd)"
+mv "Image" "AK"
 
-zip "PKernel.zip" "$AK/AK"
+zip "PKernel.zip" "AK"
